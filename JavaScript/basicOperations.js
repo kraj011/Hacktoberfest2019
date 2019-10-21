1c5a15ed076ad1241e376d4b5de8f030b0014b01
David Krajewski <davidkrajewski@yahoo.com>
2019-10-20 22:45:44 -0400

    Added basic operations js file

---
+++ JavaScript/basicOperations.js
@@ -0,0 +1,8 @@
+// Math in JS is very similar to what you would expect in most other programming languages
+let i = 1;
+let j = 2;
+console.log(i + j);
+console.log(i * j);
+console.log(i / j);
+console.log(i - j);
+console.log(i % j);
